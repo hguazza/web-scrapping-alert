c59332670797fb772ab1f566ccb8019018c799d6
hguazza <144302319+hguazza@users.noreply.github.com>
2025-04-08 14:27:40 -0300

    v3 - parse function returns timestamp and the code runs every x seconds

--- src/web-scrapping-alert/app.py
+++ src/web-scrapping-alert/app.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 def fetch_page(url):
     response = requests.get(url)
@@ -17,15 +18,21 @@ def parse_page(html):
     new_price = int(prices[1].get_text(strip=True).replace('.', ''))
     installment_price = int(prices[2].get_text(strip=True).replace('.', ''))
 
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
     return {
         'product_name': product_name,
         'old_price': old_price,
         'new_price': new_price,
-        'installment_price': installment_price
+        'installment_price': installment_price,
+        'timestamp': timestamp
     }
+
+
 
 if __name__ == "__main__":
     url = "https://www.mercadolivre.com.br/macbook-air-m2-2022-midnight-16gb-de-ram-256gb-ssd-apple-m/p/MLB29578461#polycard_client=search-nordic&searchVariation=MLB29578461&wid=MLB4020634589&position=4&search_layout=grid&type=product&tracking_id=64fa67f8-9370-40ec-8bda-f0f673393914&sid=search"
     page_content = fetch_page(url)
     product_info = parse_page(page_content)
     print(product_info)
+    time.sleep(10)
